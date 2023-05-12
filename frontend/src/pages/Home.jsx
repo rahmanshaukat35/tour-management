ca252c80089f0637e6564a8f40620d3498a8a8fc
rahmanshaukat35 <rahmanshaukat35@gmail.com>
2023-05-12 23:22:46 +0500

    new

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import Subtitle from '../shared/Subtitle';
 import worldImg from '../assets/images/world.png';
 import SearchBar from '../shared/SearchBar/SearchBar';
 import ServiceList from '../services/ServiceList';
+import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
 const Home = () => {
 	return (
 		<>
@@ -54,15 +55,32 @@ const Home = () => {
 				</Container>
 			</section>
 			{/* ========Hero section end======== */}
-			<Container>
-				<Row>
-					<Col lg='3'>
-						<h5 className='services__subtitle'>What we serve</h5>
-						<h2 className='services__title'>We offer our best services</h2>
-					</Col>
-					<ServiceList />
-				</Row>
-			</Container>
+			{/* ============services start============ */}
+			<section>
+				<Container>
+					<Row>
+						<Col lg='3'>
+							<h5 className='services__subtitle'>What we serve</h5>
+							<h2 className='services__title'>We offer our best services</h2>
+						</Col>
+						<ServiceList />
+					</Row>
+				</Container>
+			</section>
+			{/* ============services end============ */}
+			{/* =============featured tour section start====== */}
+			<section>
+				<Container>
+					<Row>
+						<Col lg='12' className='mb-5'>
+							<Subtitle subtitle={'Explore'} />
+							<h2 className='featured__tour-title'>Our featured tours</h2>
+						</Col>
+						<FeaturedTourList />
+					</Row>
+				</Container>
+			</section>
+			{/* =============featured tour section end====== */}
 		</>
 	);
 };
